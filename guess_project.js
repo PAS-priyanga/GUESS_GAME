2dad493703bbd99cc30c58caaec7b0dd879671d5
priyanga_pas <apriyangasingh@gmail.com>
2023-06-26 18:20:07 -0700

    js till word has letters now need make it a dash

--- guess_project.js
+++ guess_project.js
@@ -9,13 +9,83 @@ const levelOne= ["bite","blog","bomb","brew","chop","code","coke","deal","draw",
 //const levelEight = ["countryside", "hydroplanes", "switchboard", "nefariously", "Switzerland", "warehousing", "speculation", "sympathized", "geophysical", "atmospheric"];
 
 
+
 /*----- state variables -----*/
 let secretWord;
 let guessedLetters;
-let remainingAttempts;
+let remainingAttempts=8;
+const correctLetters = [];
 
-/*----- cached elements  -----*/
+/*----- cached elements  ----- to listen to various events*/ 
 const wordDisplay = document.getElementById("word-display");
 const guessesLeft = document.getElementById("remaining-attempts");
-const letters = document.querySelectorAll("#letters span");
+const letters = document.querySelectorAll(".letters");
 const playAgainBtn = document.getElementById("play-again");
+
+/*----- event listeners -----*/
+//sellectall for selecting letters
+//start game
+//end game
+//reset
+//instruction
+
+
+/*----- functions -----*/
+//
+// Initialize the game
+function initGame() {
+    secretWord = getRandomWord();
+    guessedLetters = [];
+    remainingAttempts = 8;
+    // Initialize the guess string with dashes
+let guessString = "-".repeat(secretWord.length);
+console.log(guessString);
+console.log(secretWord.length);
+    
+  }
+
+   //render function()
+  //   displaying dash for letters
+   
+
+  // Function to get a random word
+ 
+  function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * levelOne.length);
+    return levelOne[randomIndex];
+  }
+  // Assign random word to secretWord variable
+secretWord = getRandomWord();
+  console.log(secretWord);
+  // updating of letters guesssed
+  function updateGuessedletter(letter) {
+    if (guessedLetter(letter) === includes(secretWord)){correctLetters.push (letter)}else{remainingAttempts-1};
+    correctLetters.style.visibility = correctLetters ? "visible" : "hidden";
+   // for (let i = 0; i < word.length; i++) {
+      //if (word[i] === guess) {
+        //guessedWord[i] = guess;
+        //isCorrectGuess = true;
+      //}
+    //}
+    //if (isCorrectGuess) {
+      //correctLetters.push(guess);
+    //} else {
+      //guessedLetters.push(guess);
+   // }
+  }
+  //updating remaining guess
+  //disabling clicked letters
+
+  //other function
+  //game winning or lost by if === function
+//   console.log(remainingAttempts);
+//   if (remainingAttempts === 0) {
+//     document.getElementById('status').textContent = 'Game over! You ran out of attempts.';
+   
+//     initGame();
+//   } else if (!displayGuessedWord()){}
+
+  //restart enabling all letters
+  //message display winning or loosing
+  //level movement
+  initGame()
